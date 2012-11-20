414fde65b505e34be608ac55deb940e777a32178
Dr Nic Williams <drnicwilliams@gmail.com>
2012-11-20 09:18:27 -0800

    initial bundler repo

---
+++ lib/cloudfoundry-installer.rb
@@ -0,0 +1,7 @@
+require "cloudfoundry-installer/version"
+
+module Cloudfoundry
+  module Installer
+    # Your code goes here...
+  end
+end
---
+++ lib/cloudfoundry-installer/version.rb
@@ -0,0 +1,5 @@
+module Cloudfoundry
+  module Installer
+    VERSION = "0.0.1"
+  end
+end
